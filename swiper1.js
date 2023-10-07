22f7c67720dfcc784acb3cdbf95176d75278d9ad
Sezim Erkinbekova <uivmat3@gmail.com>
2023-10-08 05:01:01 +0600

    filess

--- swiper1.js
+++ swiper1.js
@@ -4,3 +4,5 @@ const swiper = new Swiper(".mySwiper", {
       dynamicBullets: true,
     },
   });
+
+
---
+++ swiper2.js
@@ -0,0 +1,8 @@
+const swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination2",
+      clickable: true,
+    },
+  });
